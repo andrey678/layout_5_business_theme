11c0e8513a348dac80a6db10beec663458f426ae
andrey678 <drew6496@yandex.ru>
2021-07-19 20:55:39 +0300

    Make some animation to sections Header andServices

--- #src/js/script.js
+++ #src/js/script.js
@@ -27,12 +27,52 @@ document.querySelector('body').classList.add('no-webp');
 
 // $(function(){});
 
-// $(document).ready(function(){
-// 	$('.menu__link').mouseenter(function(){
-// 		$(this).toggleClass('menu__link_active')
-// 	});
-// 	$('.menu__link').mouseleave(function(){
-// 		$(this).toggleClass('menu__link_active')
-// 	});
-// });
+
+
+
+
+
+$(document).ready(function(){
+
+//Increase logo size manualy
+	$('.logo__img').mouseenter(function(){
+		$(this).animate({'width':'60px'}, 200)
+	});
+	$('.logo__img').mouseleave(function(){
+		$(this).animate({'width':'50px'}, 200)
+	});
+
+
+//Highlighting header menu items
+	$('.menu__link').mouseenter(function(){
+		$(this).toggleClass('menu__link_active')
+	});
+	$('.menu__link').mouseleave(function(){
+		$(this).toggleClass('menu__link_active')
+	});
+
+
+//Show header "Boost up your Local business"
+
+	$('.request__top').hide().delay(3000).show(1000)
+
+
+//Slide down for inputs and button
+
+	$('input:first').hide().delay(4000).slideDown(1000)
+	$('input:eq(1)').hide().delay(5000).slideDown(1000)
+	$('input:eq(2)').hide().delay(6000).slideDown(1000)
+
+	$('.request__form').find('button').hide().delay(7000).slideDown(1000)
+
+//Increase logo size animation
+	$('.logo__img').delay(8000).animate({'width':'60px'} ,300).animate({'width':'50px'}, 300)
+
+
+//Show header Our Services and subtitle
+
+	// $('.sevices__top-content').hide().scroll().slideDown(1000)???????????????
+
+});
+
 
